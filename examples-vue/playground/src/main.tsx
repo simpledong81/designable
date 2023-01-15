2a0a74ce98ec8cbd2a40cb352b8efa8bf1288c66
yiyunwan <yyw9603@163.com>
2023-01-15 17:32:41 +0800

    fix: 删除冗余代码

--- examples-vue/playground/src/main.tsx
+++ examples-vue/playground/src/main.tsx
@@ -3,20 +3,16 @@ import { createElement } from 'react'
 import { App } from '@examples-vue/react'
 import { sources } from '@examples-vue/vue'
 import { ContentWidget } from './widgets'
-const container = document.getElementById('root')
-if (container) {
-  const root = createRoot(container)
-  root.render(
-    createElement(App, {
-      Content: () =>
-        createElement(ContentWidget, {
-          components: {
-            ...sources,
-          },
-        }),
-      sources: Object.values(sources),
-    })
-  )
-} else {
-  console.error('dom root is non-existent')
-}
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+root.render(
+  createElement(App, {
+    Content: () =>
+      createElement(ContentWidget, {
+        components: {
+          ...sources,
+        },
+      }),
+    sources: Object.values(sources),
+  })
+)
