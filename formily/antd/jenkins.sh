87a607a35b44401642be5eaf30fb6540dd6ea636
simpledong81 <simpledong@gmail.com>
2023-12-19 13:28:24 +0800

    add docker

---
+++ formily/antd/jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+profile="$1" # 暂时未使用
+version="$2"
+service="rapid"
+echo ${service}:${version}
+
+npm run build:playground
+docker build -t ${service}:${version} -f Dockerfile .
+docker tag ${service}:${version} ginza.enapir.com:1080/sol/${service}:${version}
+echo "登录Harbor"
+docker login -u admin -p \(Napir\)harbor ginza.enapir.com:1080
+echo "上传镜像"
+docker push ginza.enapir.com:1080/sol/${service}:${version}
