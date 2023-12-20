c22edd8062970c2e204bb62843b3404d3702fe18
simpledong81 <simpledong@gmail.com>
2023-12-20 16:10:13 +0800

    add version

--- formily/antd/playground/widgets/LogoWidget.tsx
+++ formily/antd/playground/widgets/LogoWidget.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useTheme } from '@rapid/designable-react'
 import logoDark from './logo/rapid-color.svg'
 import logoLight from './logo/rapid-black.svg'
+import packageJson from '../../package.json'
 
 const logo = {
   dark: logoDark,
@@ -12,7 +13,8 @@ export const LogoWidget: React.FC = () => {
   const url = logo[useTheme()]
   return (
     <div style={{ display: 'flex', alignItems: 'center', fontSize: 14 }}>
-      <img src={url} style={{ margin: '0 8px', height: 32, width: 'auto' }} />
+      <img src={url} style={{ margin: '0 8px', height: 32, width: 'auto' }} />{' '}
+      <span>v{packageJson.version}</span>
     </div>
   )
 }
